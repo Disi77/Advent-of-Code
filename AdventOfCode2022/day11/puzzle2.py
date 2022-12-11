7aaae6ddbbe79ad0ae17e32f56497492a73d891a
Disi77 <disi77@gmail.com>
2022-12-11 21:06:07 +0100

    2022 day 11

---
+++ AdventOfCode2022/day11/puzzle2.py
@@ -0,0 +1,42 @@
+from pathlib import Path
+
+
+here = Path(__file__).parent
+puzzle_input = Path(here / "input.txt").read_text().split("\n\n")
+
+# Inputs preparation
+MAGIC_NUMBER = 1
+monkeys = []
+for m in puzzle_input:
+    rows = m.split("\n")
+    items = [int(i) for i in rows[1].replace("Starting items: ", "").split(", ")]
+    formula = rows[2].replace("Operation: new = ", "").strip()
+    divisor = int(rows[3].split()[-1])
+    true_monkey = int(rows[4].split()[-1])
+    false_monkey = int(rows[5].split()[-1])
+    monkeys.append({"items": items,
+                    "formula": formula,
+                    "divisor": divisor,
+                    "true_m": true_monkey,
+                    "false_m": false_monkey,
+                    "inspected_items": 0
+                    })
+    MAGIC_NUMBER *= divisor
+
+
+# Monkey business
+for round in range(1, 10001):
+    for m in monkeys:
+        for index, i in enumerate(m["items"]):
+            _, operator, b = m["formula"].split()
+            b = i if b == "old" else int(b)
+            worry_level = i * b if operator == "*" else i + b
+            m["items"][index] = worry_level
+            m["inspected_items"] += 1
+            throw_to_monkey = m["true_m"] if worry_level % m["divisor"] == 0 else m["false_m"]
+            worry_level = worry_level % MAGIC_NUMBER
+            monkeys[throw_to_monkey]["items"].append(worry_level)
+        m["items"] = []
+
+result = sorted([m["inspected_items"] for m in monkeys])
+print("Puzzle 2 =", result[-1] * result[-2])
